f7417bb1c8177c6ebc97ca850a5a328841b5778d
Thainá Cardoso <thainacardosovalenca@gmail.com>
2022-07-27 21:55:32 -0300

    alteracoes no teste automatizado

---
+++ cypress/integration/produtos.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+
+
+describe('Funcionalidade Página de Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    it('Deve selecionar um produto da lista', () => {
+
+        cy.get('[class="product-block grid"]')
+            //.first()
+            //.last()
+            //.eq(4)
+            .contains('Aero Daily Fitness Tee')
+            .click()
+    });
+
+    it.only('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 10
+        
+        cy.get('[class="product-block grid"]')
+            .contains('Aero Daily Fitness Tee').click()
+            cy.get('.button-variable-item-XL').click()
+            cy.get('.button-variable-item-Yellow').click()
+            cy.get('.input-text').clear().type(quantidade)
+            cy.get('.single_add_to_cart_button').click()
+
+            cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+            cy.get('.woocommerce-message').should('contain', quantidade + ' × “Aero Daily Fitness Tee” foram adicionados no seu carrinho.')
+    });
+
+});
